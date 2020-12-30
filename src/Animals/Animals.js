4aa26d8bf04c2b19f2a4b1851e59491e87dd5700
Christopher Mojekwu <chris@Christophers-MacBook-Pro.local>
2020-12-30 07:47:57 -0600

    deleted unused mouse actions & comments

--- src/Animals/Animals.js
+++ src/Animals/Animals.js
@@ -70,78 +70,27 @@ function Animals(props) {
     },
   };
 
-  // graph event callbacks
-  const onClickGraph = function () {
-    window.alert(`Clicked the graph background`);
-  };
-
   const onClickNode = function (nodeId) {
-    //console.log("hey!")
     setConnect("");
     setModalData(Birds[`${nodeId}`]);
     setShowModal(true);
   };
 
-  const onDoubleClickNode = function (nodeId) {
-    window.alert(`Double clicked node ${nodeId}`);
-  };
-
-  const onRightClickNode = function (event, nodeId) {
-    window.alert(`Right clicked node ${nodeId}`);
-  };
-
-  const onMouseOverNode = function (nodeId) {
-    //window.alert(`Mouse over node ${nodeId}`);
-    //setModalData(test[`${nodeId}`])
-    //setShowModal(true);
-  };
-
-  const onMouseOutNode = function (nodeId) {
-    //window.alert(`Mouse out node ${nodeId}`);
-    //(false);
-  };
-
   const onClickLink = function (source, target) {
-    //window.alert(`Clicked link between ${source} and ${target}`);
-    //console.log("hey!")
     setModalData(Birds[`${source}`]);
     setConnect(Birds[`${target}`]);
     setDetail(Birds[`${source}`].connection[target].detail);
     setShowModal(true);
   };
 
-  const onRightClickLink = function (event, source, target) {
-    window.alert(`Right clicked link between ${source} and ${target}`);
-  };
-
-  const onMouseOverLink = function (source, target) {
-    // window.alert(`Mouse over in link between ${source} and ${target}`);
-  };
-
-  const onMouseOutLink = function (source, target) {
-    //window.alert(`Mouse out link between ${source} and ${target}`);
-  };
-
-  const onNodePositionChange = function (nodeId, x, y) {
-    // window.alert(`Node ${nodeId} is moved to new position. New position is x= ${x} y= ${y}`);
-  };
   return (
     <>
       <Graph
-        id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+        id="graph-id"
         data={data}
         config={myConfig}
         onClickNode={onClickNode}
-        onDoubleClickNode={onDoubleClickNode}
-        onRightClickNode={onRightClickNode}
-        onClickGraph={onClickGraph}
         onClickLink={onClickLink}
-        onRightClickLink={onRightClickLink}
-        onMouseOverNode={onMouseOverNode}
-        onMouseOutNode={onMouseOutNode}
-        onMouseOverLink={onMouseOverLink}
-        onMouseOutLink={onMouseOutLink}
-        onNodePositionChange={onNodePositionChange}
       />
       <Modal
         show={showModal}
